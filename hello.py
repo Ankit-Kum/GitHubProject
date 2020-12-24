607022c8e10ddf6a9f7b613535af9fb514f8b660
Ankit@gmail.com <mrankitgoyal@gmail.com>
2020-12-24 05:07:31 +0200

    Done form Tester side plz review my updated code

--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
 
-f1 = input("Enter your name")
-f2 = input("Enter your age")
-f3 = f1+f2
-print(f3)
+print("I a leraning git also")
+
+
+#f1 = input("Enter your name")
+#f2 = input("Enter your age")
+Name = input("Enter your name")
+Age = input("Enter your age")
+#f3 = f1+f2
+#print(f3)
+full_name = Name + Age
+print ("your full name is ==>" +fullname)
+
